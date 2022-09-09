4e78fffd48939a61b621a0bc0e656efda1704024
Samrith Hoeng <55125142+bronz021@users.noreply.github.com>
2022-09-09 22:47:10 +0700

    nothing

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const ejs = require("ejs");
 const port = 3000;
 const app = express();
-
+app.set("view engine", "ejs");
 app.get("/", (req, res) => {
   res.render("index", { title: "Home" });
 });
